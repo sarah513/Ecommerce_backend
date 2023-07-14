c368e29351100c5eace388bc8300dfc329b3d7fb
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-07-14 15:49:34 +0300

    Update product_model.js

--- DB/models/product_model.js
+++ DB/models/product_model.js
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 const productSchema = new Schema({
     price:{type:Number},
     newPrice:{type:Number},
+    brand:{type:String, default:""},
     isAvailable:{type:Boolean, default:true},
     prodName:{type:String , required:true},
     prodDescription:{type:String},
