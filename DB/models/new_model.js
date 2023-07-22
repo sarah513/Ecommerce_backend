64b2779af48546a4ccf18271342fd3c5bb9b382a
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-07-22 14:37:42 +0300

    Update new_model.js

--- DB/models/new_model.js
+++ DB/models/new_model.js
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose";
 const newSchema = new Schema({
-    prodID:{type: Schema.Types.ObjectId, ref: 'Product'}
+    prodID:{type: Schema.Types.ObjectId, ref: 'Product',unique:true}
 })
 
 export const newModel = model('New', newSchema)
