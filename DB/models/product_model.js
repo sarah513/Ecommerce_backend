8b49d5697aa54f18a65c70b9399dc9edbf8b5b76
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-07-23 17:23:59 +0300

    Update product_model.js

--- DB/models/product_model.js
+++ DB/models/product_model.js
@@ -11,7 +11,8 @@ const productSchema = new Schema({
     images:[{type:Object}],
     isNew:{type:Boolean,default:false},
     quantity:{type:Number, default:1},
-    Rating:{type:Number,default:5}
+    Rating:{type:Number,default:5},
+    raters:{type:Number , default:0}
    
 })
 export const ProdModel = model('Product', productSchema)
