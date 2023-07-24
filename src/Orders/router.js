9d527259a9edfcc7091129805931683043f563cc
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-07-24 13:21:35 +0300

    Update router.js

--- src/Orders/router.js
+++ src/Orders/router.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { addOrder, updateOrder } from "./controller/controller.js";
+import { addOrder, updateOrder ,getOrders, getOrderByState, deleteOrder } from "./controller/controller.js";
 const router = Router()
 // add order
 router.post('/add/:id',addOrder)
 //update order
 router.patch('/update/:id',updateOrder)
+//get orders
+router.get('/all',getOrders)
+
+//filter
+router.get('/by',getOrderByState)
+
+//dlt
+router.get('/dlt/:id',deleteOrder)
 export default router
