ecabe635c78923d94075428920bd90187918ca1d
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-08-01 09:19:58 +0300

    Update product_model.js

--- DB/models/product_model.js
+++ DB/models/product_model.js
@@ -1,27 +1,23 @@
 import { Schema, model } from "mongoose";
 const productSchema = new Schema({
-    price:{type:Number},
-    newPrice:{type:Number},
-    brand:{type:String, default:""},
-    isAvailable:{type:Boolean, default:true},
-    prodName:{type:String , required:true},
-    prodDescription:{type:String},
-    sale:{type:Number, default:0},
-    category:{type:String},
-    images:[{type:Object}],
-    isNew:{type:Boolean,default:false},
-    quantity:{type:Number, default:1},
-    Rating:{type:Number,default:0},
-    raters:[{
-        user: { type: Schema.Types.ObjectId, ref: 'User' },
+    price: { type: Number },
+    newPrice: { type: Number },
+    brand: { type: String, default: "" },
+    isAvailable: { type: Boolean, default: true },
+    prodName: { type: String, required: true },
+    prodDescription: { type: String },
+    sale: { type: Number, default: 0 },
+    category: { type: String },
+    images: [{ type: Object }],
+    quantity: { type: Number, default: 1 },
+    rate: [{
+        user: { type: Schema.Types.ObjectId, ref: "User" },
         rate: { type: Number, default: 0 }
     }],
-    comments:[
-        {
-        user: { type: Schema.Types.ObjectId, ref: 'User' },
-        comment: { type: String }
-    }
+    rating: { type: Number, default: 0 },
+    comments: [{
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        comment: { type: String } }
     ]
-   
-})
+}, { timestamps: true })
 export const ProdModel = model('Product', productSchema)
