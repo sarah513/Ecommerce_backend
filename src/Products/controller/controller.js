b9495da4157f98107fbc7c25edc0e0374dfc0f1d
sarahMustafa <62691032+sarah513@users.noreply.github.com>
2023-08-02 13:23:11 +0300

    Update controller.js

--- src/Products/controller/controller.js
+++ src/Products/controller/controller.js
@@ -4,18 +4,19 @@ import { ErrorHandler } from "../../../utils/ErrHandler.js";
 import { doneResponse } from "../../../utils/done.js";
 
 import algoliasearch from "algoliasearch";
+import { userModel } from "../../../DB/models/user_model.js";
 
 export const addProduct = ErrorHandler(async (req, res, next) => {
     let arr = []
-    let { prodName, price, prodDescription, category,brand,isAvailable,quantity } = req.body
+    let { prodName, price, prodDescription, category, brand, isAvailable, quantity } = req.body
     console.log(req.body)
-   
+
     if (req.files.length) {
         for (let file of req.files) {
             console.log(file.path)
-            let nam=req.body.prodName
-        
-            let { secure_url, public_id } = await cloudinary.uploader.upload(file.path, { folder: `products/${nam}` }).catch(err=>{next(new Error("name of product not suitable"))})
+            let nam = req.body.prodName
+
+            let { secure_url, public_id } = await cloudinary.uploader.upload(file.path, { folder: `products/${nam}` }).catch(err => { next(new Error("name of product not suitable")) })
             console.log({ secure_url, public_id })
             arr.push({ secure_url, public_id })
         }
@@ -35,7 +36,7 @@ export const addProduct = ErrorHandler(async (req, res, next) => {
 })
 export const getAllProducts = ErrorHandler(
     async (req, res, next) => {
-        let { category, price ,brand } = req.query
+        let { category, price, brand } = req.query
         console.log({ category, price })
         let query
         if (category && price) {
@@ -53,11 +54,11 @@ export const getAllProducts = ErrorHandler(
             query = {
                 price: { $gte: price[0], $lte: price[1] },
             };
-        }else if(brand){
+        } else if (brand) {
             query = {
                 brand: brand
             };
-                }
+        }
         let allProducts = query ? await ProdModel.find(query) : await ProdModel.find()
         allProducts ? doneResponse(res, allProducts) : next(new ErrorHandler('No products available at this time', { cause: 404 }))
     }
@@ -68,16 +69,17 @@ export const dltProduct = ErrorHandler(
         const { id } = req.params
         console.log(id)
         let delted = await ProdModel.findByIdAndDelete(id)
-        if(delted){
-            let arr= delted.images
-        let tr
-        console.log(arr)
-        arr.map(async ele=>{
-            tr= await cloudinary.uploader.destroy(ele.public_id, function(error,result) {
-            console.log(result, error) });
-        })
-        console.log(tr)
-        delted ? doneResponse(res, delted) : next(new Error('This product is already deleted', { cause: 404 }))
+        if (delted) {
+            let arr = delted.images
+            let tr
+            console.log(arr)
+            arr.map(async ele => {
+                tr = await cloudinary.uploader.destroy(ele.public_id, function (error, result) {
+                    console.log(result, error)
+                });
+            })
+            console.log(tr)
+            delted ? doneResponse(res, delted) : next(new Error('This product is already deleted', { cause: 404 }))
         }
 
     }
@@ -93,7 +95,7 @@ export const getProductById = ErrorHandler(
 export const updateProduct = ErrorHandler(
     async (req, res, next) => {
         let { id } = req.params
-        let { price, isAvailable, prodName, prodDescription, sale, category,brand ,isNew ,quantity} = req.body
+        let { price, isAvailable, prodName, prodDescription, sale, category, brand, isNew, quantity } = req.body
         let newPrice
         console.log(req.body)
         if (price) {
@@ -107,7 +109,7 @@ export const updateProduct = ErrorHandler(
             }
 
         }
-        let updated = await ProdModel.findByIdAndUpdate(id, { price, isAvailable, prodName, prodDescription, newPrice, sale, category,brand,isNew,quantity }, { new: true })
+        let updated = await ProdModel.findByIdAndUpdate(id, { price, isAvailable, prodName, prodDescription, newPrice, sale, category, brand, isNew, quantity }, { new: true })
         updated ? doneResponse(res, updated) : next(new Error('error in update product or product dosn`t exist', { cause: 500 }))
     }
 )
@@ -180,5 +182,74 @@ export const addImageForProduct = ErrorHandler(
         console.log(images)
         let updates = await ProdModel.findByIdAndUpdate(id, { images }, { new: true })
         updates ? doneResponse(res, updates) : next(new Error('error in adding image', { cause: 500 }))
+    }
+)
+
+export const addRate = ErrorHandler(
+    async (req, res, next) => {
+        let { uid, pid } = req.params
+        let newRating = req.body.rate
+        ProdModel.findOneAndUpdate(
+            { _id: pid, "rate.user": uid },
+            { $set: { "rate.$.rate": newRating } },
+            { new: true }
+        ).then(result => {
+            if (result) {
+                ProdModel.find({ _id: pid, "rate.rate": { $exists: true } }, { "rate.rate": 1 }).then(result => {
+                    // console.log(result)
+                    let x = 0
+                    for (let index = 0; index < result[0].rate.length; index++) {
+                        // const element = array[index];
+                        console.log(result[0].rate[index].rate)
+                        x += result[0].rate[index].rate
+                        console.log('size', result[0].rate.length)
+                    }
+                    console.log('x', x / result[0].rate.length)
+                    x = x / result[0].rate.length
+                    ProdModel.findByIdAndUpdate(pid, { rating: x }, { new: true }).then(result => {
+                        userModel.findOneAndUpdate({ _id: uid, "rate.product": pid },
+                            { $set: { "rate.$.rate": newRating } },
+                            { new: true }).then(result => doneResponse(res, result)).catch(
+                                err => next(new ErrorHandler('Error in updating rate to user'))
+                            )
+
+                    }).catch(err => next(new Error("error in calculating rating")))
+                }).catch(err => {
+                    console.log(err)
+                })
+
+            } else {
+                ProdModel.findOneAndUpdate(
+                    { _id: pid },
+                    { $addToSet: { rate: { user: uid, rate: newRating } } },
+                    { new: true }
+                ).then(result => {
+                    // Calculate the average rating and update the rating field
+                    ProdModel.find({ _id: pid, "rate.rate": { $exists: true } }, { "rate.rate": 1 }).then(result => {
+                        // console.log(result)
+                        let x = 0
+                        for (let index = 0; index < result[0].rate.length; index++) {
+                            x += result[0].rate[index].rate
+                        }
+                        x = x / result[0].rate.length
+                        ProdModel.findByIdAndUpdate(pid, { rating: x }, { new: true }).then(result => {
+                           userModel.findOneAndUpdate(
+                                { _id: uid },
+                                { $addToSet: { rate: { product: pid, rate: newRating } } },
+                                { new: true }).then(result=> doneResponse(res, result))
+                                .catch(err=>next(new Error('error in adding rate ro user')))
+                           
+                        }).catch(err => next(new Error("error in calculating rating")))
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                }).catch(err => {
+                    next(new Error('error in adding rate at product'))
+                })
+            }
+        }).catch(err => {
+            console.log(err)
+            next(new Error("error in updating rate at product"))
+        })
     }
 )
